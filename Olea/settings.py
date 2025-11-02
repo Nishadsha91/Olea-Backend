2454cdcddb6e588c05aced997013f2f08a7a9fda
Nishadsha91 <muthumattilnishad91@gmail.com>
2025-11-02 20:22:40 +0530

    complted

--- Olea/settings.py
+++ Olea/settings.py
@@ -145,10 +145,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 
 
